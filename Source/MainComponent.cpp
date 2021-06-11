59f39452270536eb7893a588c7951ad91d5feaed
KottV <kv@kott.no-ip.biz>
2021-06-11 13:36:10 +1000

    mouseEvent

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(comp);
     setSize (600, 400);
 }
 
@@ -26,4 +27,6 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+
+    comp.setBounds(30, 30, 100, 100);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,6 +2,20 @@
 
 #include <JuceHeader.h>
 
+struct MyComp : Component
+{
+    void resized() override {}
+    void paint(Grapgics& g) override { g.fillAlll(Colours::green); }
+
+    void mouseEnter(const MouseEvent& e) override
+    {
+        DBG("mouseEnter");
+    }
+    void mouseExit(const MouseEvent& e) override
+    {
+        DBG("mouseExit");
+    }
+};
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
@@ -22,6 +36,7 @@ private:
     //==============================================================================
     // Your private member variables go here...
 
+    MyComp comp;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
